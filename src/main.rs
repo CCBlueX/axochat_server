3bf301946e3c92d1467e3f750f977913900f4459
frozolotl <frozolotl@protonmail.com>
2019-03-03 16:09:27 +0100

    start actix-web server

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-mod error;
 mod config;
+mod error;
 
-use log::*;
 use error::*;
+use log::*;
+
+use actix_web::{actix::System, server::HttpServer, App};
 
 fn main() -> Result<()> {
     env_logger::init();
     let config = config::read_config()?;
     debug!("Read configuration file: {:?}", config);
+
+    let system = System::new("axochat");
+
+    HttpServer::new(move || App::new())
+        .bind(config.net.address)?
+        .start();
+
+    info!("Started server at {}", config.net.address);
+    system.run();
 
     Ok(())
 }
