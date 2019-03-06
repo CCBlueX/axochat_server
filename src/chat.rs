910afc9aeb32ff8daefdabfc2294b7b119aef176
frozolotl <frozolotl@protonmail.com>
2019-03-06 13:29:41 +0100

    allow private messages to be sent

--- src/chat.rs
+++ src/chat.rs
@@ -160,6 +160,11 @@ enum ClientPacket {
         author_name: Option<String>,
         content: String,
     },
+    PrivateMessage {
+        author_id: usize,
+        author_name: Option<String>,
+        content: String,
+    },
 }
 
 /// A serverbound packet
@@ -167,6 +172,7 @@ enum ClientPacket {
 enum ServerPacket {
     Login { username: String, anonymous: bool },
     Message { content: String },
+    PrivateMessage { receiver_id: usize, content: String },
 }
 
 #[derive(Message)]
@@ -251,6 +257,10 @@ impl Handler<ServerPacketId> for ChatServer {
                     .connections
                     .get(&user_id)
                     .expect("could not find connection");
+                if !session.username.is_empty() {
+                    debug!("{:x} tried to log in multiple times.", user_id);
+                    return;
+                }
 
                 match authenticate(&username, &session.session_hash) {
                     Ok(fut) => {
@@ -295,6 +305,10 @@ impl Handler<ServerPacketId> for ChatServer {
                     .connections
                     .get(&user_id)
                     .expect("could not find connection");
+                if session.username.is_empty() {
+                    debug!("{:x} tried to send message, but is not logged in.", user_id);
+                    return;
+                }
 
                 let author_name = if session.anonymous {
                     None
@@ -313,6 +327,40 @@ impl Handler<ServerPacketId> for ChatServer {
                         if let Err(err) = session.addr.do_send(client_packet.clone()) {
                             warn!("Could not send message to client: {}", err);
                         }
+                    }
+                }
+            }
+            ServerPacket::PrivateMessage { receiver_id, content } => {
+                info!("{:x} has written to `{:x}`.", user_id, receiver_id);
+                debug!("{:x} has written `{}` to `{:x}`.", user_id, content, receiver_id);
+                let sender_session = self
+                    .connections
+                    .get(&user_id)
+                    .expect("could not find connection");
+                if sender_session.username.is_empty() {
+                    debug!("{:x} tried to send private message, but is not logged in.", user_id);
+                    return;
+                }
+                let receiver_session = self
+                    .connections
+                    .get(&receiver_id)
+                    .expect("could not find connection");
+
+                let author_name = if sender_session.anonymous {
+                    None
+                } else {
+                    Some(sender_session.username.clone())
+                };
+
+                // check if user is actually logged in
+                if !receiver_session.username.is_empty() {
+                    let client_packet = ClientPacket::PrivateMessage {
+                        author_id: user_id,
+                        author_name,
+                        content: content,
+                    };
+                    if let Err(err) = receiver_session.addr.do_send(client_packet) {
+                        warn!("Could not send private message to client: {}", err);
                     }
                 }
             }
