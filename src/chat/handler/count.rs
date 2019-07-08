9fac21592c24725589ab8f42796c338ae4a3b887
frozolotl <frozolotl@protonmail.com>
2019-07-08 15:51:26 +0200

    require users to be moderators to retrieve the user count
    SenkJu wanted me to write this thing for some reason

--- src/chat/handler/count.rs
+++ src/chat/handler/count.rs
@@ -1,4 +1,5 @@
 use log::*;
+use crate::error::*;
 use crate::chat::{InternalId, ChatServer, ClientPacket};
 
 impl ChatServer {
@@ -7,11 +8,31 @@ impl ChatServer {
             .connections
             .get(&user_id)
             .expect("could not find connection");
-        if let Err(err) = session.addr.do_send(ClientPacket::UserCount {
-            connections: self.connections.len() as u32,
-            logged_in: self.ids.len() as u32,
-        }) {
-            warn!("Could not send user count to user `{}`: {}", user_id, err);
+
+        if let Some(info) = &session.user {
+            if !self.moderation.is_moderator(&info.uuid) {
+                info!("`{}` tried to get the user count without permission", user_id);
+                let _ = session
+                    .addr
+                    .do_send(ClientPacket::Error {
+                        message: ClientError::NotPermitted,
+                    });
+                return;
+            }
+
+            if let Err(err) = session.addr.do_send(ClientPacket::UserCount {
+                connections: self.connections.len() as u32,
+                logged_in: self.ids.len() as u32,
+            }) {
+                warn!("Could not send user count to user `{}`: {}", user_id, err);
+            }
+        } else {
+            info!("`{}` is not logged in.", user_id);
+            let _ = session
+                .addr
+                .do_send(ClientPacket::Error {
+                    message: ClientError::NotLoggedIn,
+                });
         }
     }
 }
